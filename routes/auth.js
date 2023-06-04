e40db519daa859b357897215f2750b6fc9cbd1b0
sunshine-web-coder <webcoderdev02@gmail.com>
2023-06-04 04:19:24 +0100

    first commit

---
+++ routes/auth.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const User = require('../models/UserModels');
+
+// User registration
+router.post('/register', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Check if the username already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'Username already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create a new user
+    const newUser = new User({ username, password: hashedPassword });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error('User registration error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// User login
+router.post('/login', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    // Find the user by username
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Compare the provided password with the stored password
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Generate a JSON Web Token (JWT)
+    const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+
+    res.json({ token });
+  } catch (error) {
+    console.error('User login error:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Get all users
+router.get('/', async (req, res) => {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (error) {
+      console.error('Get all users error:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+  // Get user by ID
+  router.get('/:id', async (req, res) => {
+    try {
+      const user = await User.findById(req.params.id);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (error) {
+      console.error('Get user by ID error:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+  // Update user by ID
+  router.put('/:id', async (req, res) => {
+    try {
+      const { username, password } = req.body;
+  
+      // Hash the new password
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        { username, password: hashedPassword },
+        { new: true }
+      );
+  
+      if (!updatedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+  
+      res.json(updatedUser);
+    } catch (error) {
+      console.error('Update user error:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+  
+  // Delete user by ID
+  router.delete('/:id', async (req, res) => {
+    try {
+      const deletedUser = await User.findByIdAndDelete(req.params.id);
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json({ message: 'User deleted successfully' });
+    } catch (error) {
+      console.error('Delete user error:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  });
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+// const path = require("path");
+const authRoutes = require("./routes/auth");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// MongoDB connection
+connectDB();
+
+// Middleware to protect routes that require authentication
+const requireAuth = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({ message: "Authorization token is required" });
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decodedToken) => {
+    if (err) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    req.userId = decodedToken.userId;
+    next();
+  });
+};
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Welcome to the API");
+});
+
+// app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.use("/api/auth", authRoutes);
+
+app.get("/api/protected", requireAuth, (req, res) => {
+  res.json({ message: "Protected route accessed successfully" });
+});
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+// });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
